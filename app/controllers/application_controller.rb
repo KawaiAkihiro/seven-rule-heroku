8a91e31864d2ee0191ee14c96cc657ba48cc7e29
河合彰紘 <rakko1124@gmail.com>
2021-08-30 16:14:07 +0900

    442エラーに効く文言を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
     include MastersHelper
     include SessionsHelper
     include IndividualShiftsHelper
